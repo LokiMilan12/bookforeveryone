1e2f287e6eb106c34acc8b0c12c471e941df7381
Loki <ukazaminje@gmail.com>
2023-12-22 00:01:01 +0100

    resene migracije i za pivot tabelu za autore i knjige

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ namespace Database\Seeders;
 use App\Models\Author;
 use App\Models\Book;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,11 +19,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /**
+         * book seeder
+         */
         // \App\Models\Book::factory(1000)->create();
 
+        /**
+         * author seeder
+         */
         // \App\Models\Author::factory(1000)->create();
 
-        $bookIds = Book::pluck('id')->all();
-        $authorIds = Author::pluck('id')->all();
+        /**
+         * author_book pivot table seeder start
+         */
+        // $faker = Faker::create();
+        // $bookIds = Book::pluck('id')->all();
+        // $authorIds = Author::pluck('id')->all();
+        
+        // foreach($bookIds as $bookId) {
+        //     $numberOfAuthors = $faker->numberBetween(1, 3);
+
+        //     $selectedAuthors = $faker->randomElements($authorIds, $numberOfAuthors);
+        //         foreach ($selectedAuthors as $authorId) {
+        //             DB::table('author_book')->insert([
+        //                 'book_id' => $bookId,
+        //                 'author_id' => $authorId,
+        //             ]); 
+        //         }   
+        //     }
+        /**
+         * author_book pivot table seeder end
+         */
+
+        }
     }
-}
